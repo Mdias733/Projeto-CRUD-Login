c88ce9c0f1ff910285e0d83cde107f4f9a6e7603
Matheus Dias de Souza <111648189+Mdias733@users.noreply.github.com>
2023-04-14 16:51:03 -0300

    Add files via upload

--- bancoDeDados.sql
+++ bancoDeDados.sql
@@ -1,5 +1,5 @@
-create database metalcoin;
-use metalcoin;
+create database catraca;
+use catraca;
 
 
 create table tb_cadastro(
@@ -9,6 +9,16 @@ quantidade_produto varchar(255),
 tempo_produto varchar(255)
 );
 
+create table tb_administradores(
+id_adm int primary key auto_increment,
+nome_adm varchar(255)unique,
+usuario_adm varchar(255),
+email_adm varchar(255),
+senha_adm varchar(80),
+dataCad_adm TIMESTAMP DEFAULT(CURRENT_TIMESTAMP())
+);
+insert into tb_administradores(nome_adm,usuario_adm,email_adm,senha_adm) VALUES ('matheus','a','matheus@aa.com','123');
+
 create table tb_cadUsuario(
 id_Usuario int primary key auto_increment,
 nome_Usuario varchar(255),
@@ -17,6 +27,8 @@ email_Usuario varchar(255),
 senha_Usuario varchar(80),
 dataCad_Usuario TIMESTAMP DEFAULT(CURRENT_TIMESTAMP())
 );
+insert into tb_cadUsuario(nome_Usuario,email_Usuario,senha_Usuario) VALUES ('matheus','matheus@aa.com','123');
+
 
 create table tb_reclamacao(
 id_reclamacao int primary key auto_increment,
@@ -24,7 +36,7 @@ texto_reclamacao varchar(255),
 nome_reclamacao varchar(255)
 );
 
-select*from tb_cadUsuario;
+select*from tb_administradores;
 
-drop table tb_reclamacao;
+drop table tb_administradores;
 
