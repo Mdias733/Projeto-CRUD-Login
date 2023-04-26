2f184c12029b79b1743a2621d23749803fdf375a
Matheus Dias de Souza <111648189+Mdias733@users.noreply.github.com>
2023-04-26 14:59:30 -0300

    Update bancoDeDados.sql

--- bancoDeDados.sql
+++ bancoDeDados.sql
@@ -13,17 +13,6 @@ CREATE TABLE tb_administradores (
 INSERT INTO tb_administradores (nome_adm, usuario_adm, email_adm, senha_adm) 
 VALUES ('matheus', 'a', 'matheus@aa.com', '123');
 
-CREATE TABLE tb_cadUsuario (
-  id_Usuario INT PRIMARY KEY AUTO_INCREMENT,
-  nome_Usuario VARCHAR(255) UNIQUE,
-  dtNasc_Usuario VARCHAR(15),
-  email_Usuario VARCHAR(255),
-  senha_Usuario VARCHAR(80),
-  dataCad_Usuario TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
-);
-
-INSERT INTO tb_cadUsuario (nome_Usuario, email_Usuario, senha_Usuario) 
-VALUES ('1', 'matheus@aa.com', '123');
 
 CREATE TABLE tb_funcionarios (
   id_funcionario INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -34,7 +23,7 @@ CREATE TABLE tb_funcionarios (
 );
 
 INSERT INTO tb_funcionarios (nome_funcionario, email_funcionario, telefone_funcionario) 
-VALUES ('Matheus', 'matheus@aa.com', '1111123');
+VALUES ('Matheus Dias de Souza', 'matheus@aa.com', '1111123');
 
 CREATE TABLE tb_entradas (
   id_entrada INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -54,12 +43,11 @@ CREATE TABLE tb_saidas (
   CONSTRAINT FK_saidas_funcionario FOREIGN KEY (id_saida) REFERENCES tb_funcionarios(id_funcionario)
 );
 
-insert into tb_entradas values();
+insert into tb_saidas values();
 
 
-SELECT a.nome_funcionario, b.dataEHoraEntrada, c.dataEHorasaida 
-FROM tb_funcionarios a 
-INNER JOIN tb_entradas b ON a.id_funcionario = b.id_funcionario 
-INNER JOIN tb_saidas c ON a.id_funcionario = c.id_funcionario;
+
+SELECT * FROM catraca.tb_funcionarios a inner join tb_entradas b on a.id_funcionario = id_entrada
+inner join tb_saidas c on a.id_funcionario = id_saida;
 
 DROP TABLE tb_funcionarios;
